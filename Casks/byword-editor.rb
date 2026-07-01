fb5fff226d1076502f1d81d039a86341f62c9152
Felipe Alves <felipe.alves@laborit.com.br>
2026-07-01 18:00:54 -0300

    fix(cask): update byword-editor 1.0.0 sha256 from release

--- Casks/byword-editor.rb
+++ Casks/byword-editor.rb
@@ -1,6 +1,6 @@
 cask "byword-editor" do
   version "1.0.0"
-  sha256 "5776fb3c9637be797f93b1cc07bd36cd5b96c64c3dbeed31b50881607c8b9f3c"
+  sha256 "8bc7e708bdb87eafce7db32723977c6e600109edc8666a5ffbd04d38fd91a5c8"
 
   url "https://github.com/laboritdev/byword-editor/releases/download/v1.0.0/BywordEditor-1.0.0-macos-arm64.zip"
   name "BywordEditor"
