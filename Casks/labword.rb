030e783a204c0a8a175482b2977eafaf29fdf3bd
felipelaborit <143533602+felipelaborit@users.noreply.github.com>
2026-07-02 16:19:54 -0300

    chore(cask): bump labword to 1.0.6

--- Casks/labword.rb
+++ Casks/labword.rb
@@ -1,8 +1,8 @@
 cask "labword" do
-  version "1.0.5"
-  sha256 "772c20dbe0d99dae1f70a032c27a6b144ea8b858913a624761f48fb9fca454b8"
+  version "1.0.6"
+  sha256 "148b9311200505871d045d21214dd1eeeb2ddee1e4bf5b5cd1c3c62ef571c6ca"
 
-  url "https://github.com/laboritdev/labword/releases/download/v1.0.5/LabWord-1.0.5-macos-arm64.zip"
+  url "https://github.com/laboritdev/labword/releases/download/v1.0.6/LabWord-1.0.6-macos-arm64.zip"
   name "LabWord"
   desc "Minimalist Markdown editor for macOS by Laborit"
   homepage "https://github.com/laboritdev/labword"
